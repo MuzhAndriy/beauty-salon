f322f249cbb90e54b3f01676f017d895879b9b8e
Muzh Andriy <muzhandriy@gmail.com>
2019-03-06 13:43:26 +0200

    some commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 
-import './libs/mmenu/jquery.mmenu.all'
+import './libs/mmenu/jquery.mmenu.all';
 import './js/common';
 import './scss/main.scss';
--- src/js/common.js
+++ src/js/common.js
@@ -1,10 +1,18 @@
-$(document).ready(function(){
-$('.hamburger').on('click', function(ev){
-    $('.hamburger__line').toggleClass('hamburger__line_active');
-})
+$(document).ready(function () {
+    $('#my-menu').mmenu({
+        extensions: ["theme-black", "border-none", "pagedim-black", "position-right", "fx-menu-slide", "offcanvas", "border-none"],
+        navbar: {
+            title: "<img src='img/logo-1.svg' alt='Салон S&Mitler'>"
+        }
+    }, {
+        language: "ru",
 
-
-
-
-})
-
+    });
+    var api = $('#my-menu').data("mmenu");
+    api.bind('open:start', function () {
+        $('.hamburger__line').addClass('hamburger__line_active');
+    })
+    api.bind('close:finish', function () {
+        $('.hamburger__line').removeClass('hamburger__line_active');
+    })
+});
