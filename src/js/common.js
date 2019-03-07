603ffd2d44fc3e514f2bd9d0d32af9e3a7242a22
muzhandriy <45791204+muzhandriy@users.noreply.github.com>
2019-03-07 14:22:58 +0200

    resolve bug

--- src/js/common.js
+++ src/js/common.js
@@ -1,15 +1,12 @@
 $(document).ready(function () {
     $('#my-menu').mmenu({
-        extensions: ["theme-black", "border-none", "pagedim-black", "position-right", "fx-menu-slide", "offcanvas", "border-none"],
+        extensions: ["theme-black", "border-none", "pagedim-black", "position-right", "fx-menu-slide", "offcanvas"],
         navbar: {
             title: "<img src='img/logo-1.svg' alt='Салон S&Mitler'>"
-        }
-    }, {
-        language: "ru",
-
+        },
     });
     var api = $('#my-menu').data("mmenu");
-    api.bind('open:start', function () {
+    api.bind('open:finish', function () {
         $('.hamburger__line').addClass('hamburger__line_active');
     })
     api.bind('close:finish', function () {
